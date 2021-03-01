9f0166cc560443e180b531ef1d43a08ec02f151b
Yriy Yupiter <yu.piter.gm@gmail.com>
2021-03-01 07:58:59 +0300

    first module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let x = 1;
+
+  if (n <= 1) {
+    return x;
+  } else {
+    while (n > 1) {
+      x = x * n;
+      n = n - 1;
+    }
+    return x;
+  }
 }
