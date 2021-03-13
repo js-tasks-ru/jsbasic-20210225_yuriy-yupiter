9c3f9362b409afd3e2c534082eb46977cecb4fab
Yuriy Yupiter <yu.piter.gm@gmail.com>
2021-03-13 19:47:58 +0300

    module 4 tasks 1, 2, 3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+
+  let nameFriend = friends.map(({firstName, lastName}) => `${firstName} ${lastName}`)
+    .forEach(item => {
+      const li = document.createElement('li');
+      li.textContent = item;
+      ul.appendChild(li);
+    });
+
+  return ul;
 }
