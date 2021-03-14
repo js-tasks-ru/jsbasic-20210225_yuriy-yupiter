a9f7241ba0569ce83cbab2682fdc750fe8f85709
Yuriy Yupiter <yu.piter.gm@gmail.com>
2021-03-15 00:16:34 +0300

    module 4 tasks 1, 2, 3 ver 2.1

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,5 @@
 function makeDiagonalRed(table) {
   [...table.rows].forEach( (row, i) => {
-    console.log(row);
-    row.cells[i].style.backgroundColor = 'red';
+      row.cells[i].style.backgroundColor = 'red';
   });
 }
