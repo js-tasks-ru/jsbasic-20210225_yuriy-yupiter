4c2e3b9b4bb020a581e29e0f801c2985000e60f8
Yuriy Yupiter <yu.piter.gm@gmail.com>
2021-04-03 19:41:43 +0300

    module 7 task 1, 2, 3, 4 ver.1.0

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,110 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+
+    this.render();
+    this.addEventListeners();
+    this.value = '';
   }
+
+  render() {
+    this.elem = createElement(`
+              <div class="ribbon">
+                <button class="ribbon__arrow ribbon__arrow_left">
+                  <img src="../assets/images/icons/angle-icon.svg" alt="icon" />
+                </button>
+                <nav class="ribbon__inner">${this.createInnerElem( {categories: this.categories} )}</nav>
+                <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+                  <img src="../assets/images/icons/angle-icon.svg" alt="icon" />
+                </button>
+              </div>
+            `);
+
+    this.sub('item').classList.add('ribbon__item_active');
+  }
+
+  createInnerElem({categories}) {
+    return categories.map(category =>
+                    `
+                    <a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>
+                    `).join('');
+  }
+
+  addEventListeners() {
+    this.sub('arrow_left').onclick = this.onArrowLeftClick;
+    this.sub('arrow_right').onclick = this.onArrowRightClick;
+
+    this.elem.onclick = (event) => {
+      let itemElem = event.target.closest('.ribbon__item');
+      if (itemElem) {
+        this.onItemClick(itemElem);
+        event.preventDefault();
+      }
+    };
+
+    this.sub('inner').onscroll = this.onScroll;
+  }
+
+  onArrowRightClick = () => {
+    let scrolling = 350;
+    this.sub('inner').scrollBy(scrolling, 0);
+    this.updateArrows();
+  }
+
+  onArrowLeftClick = () => {
+    let scrolling = 350;
+    this.sub('inner').scrollBy(-scrolling, 0);
+    this.updateArrows();
+  }
+
+  onItemClick(itemElem) {
+    let previouslyActive = this.sub('item_active');
+    if (previouslyActive) {
+      previouslyActive.classList.remove('ribbon__item_active');
+    }
+
+    itemElem.classList.add('ribbon__item_active');
+
+    this.value = itemElem.dataset.id;
+
+    this.elem.dispatchEvent(
+      new CustomEvent('ribbon-select', {
+        detail: this.value,
+        bubbles: true,
+      })
+    );
+  }
+
+  onScroll = () => {
+    this.updateArrows();
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.ribbon__${ref}`);
+  }
+
+  scrollRight() {
+    return this.sub('inner').scrollWidth - (this.sub('inner').scrollLeft + this.sub('inner').clientWidth);
+  }
+
+  scrollLeft() {
+    return this.sub('inner').scrollLeft;
+  }
+
+  updateArrows() {
+    if (this.scrollLeft() > 0) {
+      this.sub('arrow_left').classList.add('ribbon__arrow_visible');
+    } else {
+      this.sub('arrow_left').classList.remove('ribbon__arrow_visible');
+    }
+
+    let scrollRight = this.scrollRight();
+    scrollRight = scrollRight < 1 ? 0 : scrollRight;
+    if (scrollRight > 0) {
+      this.sub('arrow_right').classList.add('ribbon__arrow_visible');
+    } else {
+      this.sub('arrow_right').classList.remove('ribbon__arrow_visible');
+    }
+  }
+
 }
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,121 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.sliderSegments = steps - 1;
+    this.value = value;
+
+    this.render();
+    this.addEventListeners();
+    this.setValue(this.value);
   }
+
+  render() {
+    function createElement(html) {
+      const div = document.createElement('div');
+      div.innerHTML = html;
+      return div.firstElementChild;
+    }
+    this.elem = createElement(`
+                                    <div class="slider">
+                                      <div class="slider__thumb">
+                                        <span class="slider__value"></span>
+                                      </div>
+                                      <div class="slider__progress"></div>
+                                      <div class="slider__steps">
+                                        ${'<span></span>'.repeat(this.steps)}
+                                      </div>
+                                    </div>
+                                  `);
+  }
+
+  setValue(value) {
+
+    let valuePercents = (value / this.sliderSegments) * 100;
+
+    this.sub('thumb').style.left = this.sub('progress').style.width = `${valuePercents}%`;
+
+    this.sub('value').innerHTML = value;
+
+    if (this.sub('step-active')) {
+      this.sub('step-active').classList.remove('slider__step-active');
+    }
+
+    this.sub('steps').querySelectorAll('span')[value].classList.add('slider__step-active');
+    return value;
+  }
+
+  addEventListeners() {
+    this.elem.onclick = this.onClick;
+    this.sub('thumb').ondragstart = () => false;
+    this.sub('thumb').onpointerdown = this.onPointerDown;
+  }
+
+  onClick = event => {
+    let positionLeft = (event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth;
+
+    this.value = this.setValue(Math.round(this.sliderSegments * positionLeft));
+
+    this.elem.dispatchEvent(
+      new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      })
+    );
+  }
+
+  onPointerDown = event => {
+    event.preventDefault();
+
+    this.elem.classList.add('slider_dragging');
+
+    document.addEventListener('pointermove', this.onPointerMove);
+    document.addEventListener('pointerup', this.onPointerUp);
+  }
+
+  onPointerMove = event => {
+    event.preventDefault();
+
+    let positionLeft = this.calcLeftCoordinates(event);
+
+    this.sub('thumb').style.left = this.sub('progress').style.width = `${positionLeft * 100}%`;
+
+    this.value = Math.round(this.sliderSegments * positionLeft);
+    this.sub('value').innerHTML = this.value;
+
+    if (this.sub('step-active')) {
+      this.sub('step-active').classList.remove('slider__step-active');
+    }
+
+    this.sub('steps').children[this.value].classList.add('slider__step-active');
+  };
+
+  calcLeftCoordinates(event) {
+    let positionLeft = (event.clientX - this.elem.getBoundingClientRect().left) / this.elem.offsetWidth;
+
+    if (positionLeft < 0) { positionLeft = 0; }
+    if (positionLeft > 1) { positionLeft = 1; }
+
+    return positionLeft;
+  }
+
+  onPointerUp = () => {
+    document.removeEventListener('pointermove', this.onPointerMove);
+    document.removeEventListener('pointerup', this.onPointerUp);
+
+    this.elem.classList.remove('slider_dragging');
+
+    this.elem.dispatchEvent(
+      new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      })
+    );
+  };
+
+  sub(ref) {
+    return this.elem.querySelector(`.slider__${ref}`);
+  }
+
 }
